0cf444a23cedf621d5e51a8ca9d119b297cd10fd
Vector1331 <tpgus4796@gmail.com>
2022-11-15 20:44:55 +0900

    [Fix] 커뮤니티 게시글 작성 content-type 수정 (김세현)

--- src/main/java/com/chaebbi/community/api/PostingApiController.java
+++ src/main/java/com/chaebbi/community/api/PostingApiController.java
@@ -17,11 +17,14 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
 
 
@@ -40,34 +43,24 @@ public class PostingApiController {
      * [Post] 31-1 게시글 작성 API
      */
     @ApiOperation(value = "[POST] 31-1 게시글 작성 ", notes = "제목, 글내용, 이미지들을 넣어 게시글을 등록합니다")
-    @PostMapping("/{userIdx}")
+    @PostMapping(value = "/{userIdx}" , consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<Void> uploadPost(@PathVariable(value = "userIdx") Long userIdx,
-                                           @ApiParam(value = "이미지파일 리스트") @RequestPart (value= "multipartFileList", required = false) List<MultipartFile> multipartFileList,
-                                           @ApiParam(value = "게시글 제목과 내용 dto") @RequestPart(value="posting") PostingDto postingDto ) throws IOException {
+                                           @ApiParam(value = "이미지파일 리스트") @RequestParam(value= "multipartFileList", required = false) List<MultipartFile> multipartFileList,
+                                           @ApiParam(value = "게시글 제목") @RequestParam(value="title", required =true) String title,
+                                           @ApiParam(value = "게시글 내용") @RequestParam(value="content", required = true) String content
+    ) throws IOException {
 
         log.info("POST 31-1 /posting/{userIdx}");
-
         userValidationController.validateuser(userIdx);
-        postValidationController.validationPost(postingDto.getContent(), postingDto.getTitle());
+        postValidationController.validationPost(content, title);
 
         Posting post = new Posting();
-        post = postingService.create(userIdx, postingDto.getContent(), postingDto.getTitle());
+        post = postingService.create(userIdx, content, title);
         postingService.save(post);
         Long postIdx = post.getIdx();
 
-        int img_rank = 1;
-
-        for(int i = 0; i < multipartFileList.size(); i++) {
-            MultipartFile multipartFile = multipartFileList.get(i);
-            String img_url = "empty";
-            if(multipartFile != null) {
-                if(!multipartFile.isEmpty()) {
-                    img_url = s3Uploader.upload(multipartFile, "static");
-                    Images images = imagesService.create(postIdx, img_url, img_rank);
-                    imagesService.save(images);
-                    img_rank++;
-                }
-            }
+        if(multipartFileList != null) {
+            imagesService.uploadImages(postIdx, multipartFileList);
         }
 
         return ResponseEntity.ok().build();
